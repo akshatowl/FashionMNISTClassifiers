a1c3827815b85f611125d35360695b68d34558db
akshatowl <akshatpandeyplus41@gmail.com>
2024-11-16 14:52:58 -0600

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // Import your CSS for styling
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <h1 className="title">Classifiers for the Fashion MNIST Datasets</h1>
+      <img src="/fashionmnist.png" alt="Fashion MNIST" className="image" />
+
+      <p className="description">
+       This blog is a documentation and summary of the various models we tried to peform a classification task using the FashionMNIST dataset. We dive into why we
+       chose the particular models we experimented with and how we gradually converged to the optimal solution according to us.
+      </p>
+      {/* New Section: Using Deep Learning: Convolutional Neural Networks */}
+      <section className="deep-learning-section">
+        <h2 className="section-title">Using Deep Learning: Convolutional Neural Networks</h2>
+        <p className="section-text">
+        Convolutional Neural Networks are a revolution in the deep learning field and especially in image recognition. 
+        The CNN is designed in such a way that it will automatically learn and adapt the feature hierarchies from the images at the spatial level. 
+        It works on the visual performances of object detection, image classification, and image segmentation.In general, CNNs are comprised of 
+        convolutional layers, pooling layers, and fully connected layers. Among these, the convolutional layers take care of feature extraction,
+        while the pooling layers reduce dimensionality and computational cost. The fully connected layers, finally, make a prediction based on learned features.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </section>
+      <p/><p/><p/><p/><p/><p/><p/><p/><p/><p/><p/><p/><p/><p/><p/>
+      <footer className="footer">
+        <p className="footer-text">Authors: Akshat Pandey, Logan Garcia, Vishnu, Pragya, Tanmay</p>
+      </footer>
     </div>
+
   );
 }
 
